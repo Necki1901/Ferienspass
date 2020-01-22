ca661e2abee6afc54501e3326d46f7c055bb4c2c
david.kunesch <david.kunesch@E3C18>
2020-01-22 11:08:36 +0100

    All Panels wihout Datetime bugs and info-labels also fixed.

--- TemplateProject/Ferienspaß/Ferienspaß/Pages/Admin_User_View.aspx.cs
+++ TemplateProject/Ferienspaß/Ferienspaß/Pages/Admin_User_View.aspx.cs
@@ -21,6 +21,8 @@ namespace Ferienspaß
         protected void Page_Load(object sender, EventArgs e)
         {
             lblInfo.Text = "";
+            lblInfo2.Text = "";
+            lblInfoBottom.Text = "";
             Fill_gvAdminUsers();
             
             if (!Page.IsPostBack)
@@ -146,13 +148,7 @@ namespace Ferienspaß
                 Response.Redirect(String.Format("Admin_Child_Administration.aspx?id={0}", userID));
             }
         }
-
-        private string GetTextForDdl(int svalue)//Verhindert Leeres Feld in DDL, wenn neuer DS hinzugefügt wird.
-        {
-            DataTable dt = db.Query("SELECT DESCRIPTION FROM usergroup WHERE UGID = ?", svalue);
-            return dt.Rows[0]["DESCRIPTION"].ToString();
-        }
-
+      
         protected void gvAdminUsers_RowEditing(object sender, GridViewEditEventArgs e)
         {
             pnlBlockBg.Visible = true;
@@ -175,7 +171,6 @@ namespace Ferienspaß
             ddlEmailConfirmed2.SelectedValue = dt.Rows[0]["EmailConfirmed"].ToString();
         }
      
-
         public string PwdResetHash(string firstname, string email, string uId)
         {
             string toEncode = firstname + email + uId + DateTime.Now;
@@ -188,11 +183,7 @@ namespace Ferienspaß
            
             if (txtGivenName.Text == "" || txtSurName.Text == "" || txtPhone.Text == "" ||txtEMail.Text == "" || ddlLocked.SelectedValue == null || ddlEmailConfirmed.SelectedValue == null || ddlUserGroup.SelectedValue==null) { valid = false; errorDescription += "Einer oder mehrere der Werte sind leer!  "; }
             else
-            {
-                ////proof integer values
-                //if ((!int.TryParse((e.NewValues["LOCKED"].ToString()), out int a)) || (e.NewValues["LOCKED"].ToString()!="1" && e.NewValues["LOCKED"].ToString() != "0" )) { valid = false; errorDescription += "ZUSTAND-Format ist ungültig!  "; }
-                //if ((!int.TryParse((e.NewValues["EmailConfirmed"].ToString()), out int b))|| (e.NewValues["EmailConfirmed"].ToString() != "1" && e.NewValues["EmailConfirmed"].ToString() != "0")) { valid = false; errorDescription += "MAIL-ZUSTAND-Format ist ungültig!  "; }
-
+            {                
                 //proof string values
                 if(txtGivenName.Text.Length>50 || txtSurName.Text.Length > 50 || txtPhone.Text.Length > 20 || txtEMail.Text.Length > 70) { valid = false; errorDescription += "NAME, TELEFON oder MAIL-Format ist ungültig!  "; }
 
@@ -213,6 +204,7 @@ namespace Ferienspaß
 
             }
             lblInfo.Text = errorDescription;
+            lblInfo2.Text = errorDescription;
             return valid;
         }
 
@@ -224,15 +216,11 @@ namespace Ferienspaß
             if (txtGivenName2.Text == "" || txtSurName2.Text == "" || txtPhone2.Text == ""|| txtEMail2.Text == ""|| ddlLocked2.SelectedValue == null || ddlEmailConfirmed2.SelectedValue == null || ddlUserGroup2.SelectedValue == null) { valid = false; errorDescription += "Einer oder mehrere der Werte sind leer!  "; }
             else
             {
-                ////proof integer values
-                //if ((!int.TryParse((e.NewValues["LOCKED"].ToString()), out int a)) || (e.NewValues["LOCKED"].ToString()!="1" && e.NewValues["LOCKED"].ToString() != "0" )) { valid = false; errorDescription += "ZUSTAND-Format ist ungültig!  "; }
-                //if ((!int.TryParse((e.NewValues["EmailConfirmed"].ToString()), out int b))|| (e.NewValues["EmailConfirmed"].ToString() != "1" && e.NewValues["EmailConfirmed"].ToString() != "0")) { valid = false; errorDescription += "MAIL-ZUSTAND-Format ist ungültig!  "; }
-
+               
                 //proof string values
                 if (txtGivenName2.Text.Length > 50 || txtSurName2.Text.Length > 50 || txtPhone2.Text.Length > 20 || txtEMail2.Text.Length > 70) { valid = false; errorDescription += "NAME, TELEFON oder MAIL-Format ist ungültig!  "; }
 
                 //proof email
-
                 if (!(txtEMail2.Text.Contains("@"))) { valid = false; errorDescription += "MAIL-Format ist ungültig!  "; }
 
                 if (Convert.ToBoolean(ViewState["isAdding"]) == true)
@@ -248,6 +236,7 @@ namespace Ferienspaß
 
             }
             lblInfo.Text = errorDescription;
+            lblInfo2.Text = errorDescription;
             return valid;
         }
 
@@ -259,12 +248,10 @@ namespace Ferienspaß
         protected void gvAdminUsers_RowDeleting(object sender, GridViewDeleteEventArgs e)
         {
             GridViewRow row = gvAdminUsers.Rows[e.RowIndex];
-
             string userID = ((Label)row.FindControl("lblItemTemplateUserID")).Text;
             db.Query($"delete from user where UID = {userID}");
-
             Fill_gvAdminUsers();
-            lblInfo.Text += "Datensatz wurde gelöscht!";
+            lblInfoBottom.Text += "Datensatz wurde gelöscht!";
         }
 
         protected void btnChildren_Click(object sender, ImageClickEventArgs e)
@@ -293,11 +280,11 @@ namespace Ferienspaß
                 {
                     if (db.ExecuteNonQuery("INSERT INTO user (GN, SN, PHONE, EMAIL, LOCKED, EmailConfirmed, UGID) Values(?,?,?,?,?,?,?)", txtGivenName.Text, txtSurName.Text, txtSurName.Text, txtEMail.Text, ddlLocked.SelectedValue, ddlEmailConfirmed.SelectedValue, ddlUserGroup.SelectedValue) > 0)//Keine Newvalues mehr sondern Bootstrap pop up
                     {
-                        lblInfo.Text = $"<span class='success'> Datensatz hinzugefügt! </span>";
+                        lblInfoBottom.Text = $"<span class='success'> Datensatz hinzugefügt! </span>";
                     }
                     else
                     {
-                        lblInfo.Text = $"<span class='error'> Nichts passiert! </span>";
+                        lblInfoBottom.Text = $"<span class='error'> Nichts passiert! </span>";
                     }
                     Fill_gvAdminUsers();
                     ViewState["isAdding"] = false;
@@ -319,11 +306,11 @@ namespace Ferienspaß
                 {
                     if (db.ExecuteNonQuery("UPDATE user SET GN = ?, SN = ?, PHONE = ?, EMAIL = ?, UGID = ?, LOCKED = ?, EmailConfirmed = ? WHERE UID = ?", txtGivenName2.Text, txtSurName2.Text, txtPhone2.Text, txtEMail2.Text, ddlUserGroup2.SelectedValue, ddlLocked2.SelectedValue, ddlEmailConfirmed2.SelectedValue, id) > 0)
                     {
-                        lblInfo.Text = $"<span class='success'> Datensatz aktualisiert! </span>";
+                        lblInfoBottom.Text = $"<span class='success'> Datensatz aktualisiert! </span>";
                     }
                     else
                     {
-                        lblInfo.Text = $"<span class='error'> Nichts passiert! </span>";
+                        lblInfoBottom.Text = $"<span class='error'> Nichts passiert! </span>";
                     }                    
                     pnlBlockBg.Visible = false;
                     pnlUpdate.Visible = false;
@@ -342,19 +329,18 @@ namespace Ferienspaß
             gvAdminUsers.DataBind();
         }
 
-        private void Fill_ddlUserGroup()
-        {
-            CsharpDB db = new CsharpDB();
-            List<int> usergroups = new List<int>();
-            DataTable dt = GetAllUserGroups();
-            ddlUserGroup.Items.Add("Alle");
-            for (int i = 0; i < dt.Rows.Count; i++)
-            {
-                //ddl.Items.Add(Convert.ToString(dt.Rows[i].ItemArray[0]));
-                ddlUserGroup.Items.Add(new ListItem(dt.Rows[i]["DESCRIPTION"].ToString(), dt.Rows[i]["UGID"].ToString()));
-            }
-        }
-
+        //private void Fill_ddlUserGroup()
+        //{
+        //    CsharpDB db = new CsharpDB();
+        //    List<int> usergroups = new List<int>();
+        //    DataTable dt = GetAllUserGroups();
+        //    ddlUserGroup.Items.Add("Alle");
+        //    for (int i = 0; i < dt.Rows.Count; i++)
+        //    {
+        //        //ddl.Items.Add(Convert.ToString(dt.Rows[i].ItemArray[0]));
+        //        ddlUserGroup.Items.Add(new ListItem(dt.Rows[i]["DESCRIPTION"].ToString(), dt.Rows[i]["UGID"].ToString()));
+        //    }
+        //}
 
         protected void btnSearch_Click(object sender, EventArgs e)
         {
